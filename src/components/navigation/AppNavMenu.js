6c0856e119f1c6e4123d3565a825cd179abffa5f
Hari <harikrishkk@gmail.com>
2022-09-07 19:10:15 -0400

    commit 7: add hooks, user profile component

--- src/components/navigation/AppNavMenu.js
+++ src/components/navigation/AppNavMenu.js
@@ -14,6 +14,7 @@ import {
   hideOnMobile,
 } from '@styles/styles';
 import SearchBar from './SearchBar';
+import UserProfile from '@components/UserProfile';
 
 const AppNavMenu = (props) => {
   return (
@@ -37,6 +38,9 @@ const AppNavMenu = (props) => {
           </Box>
           <Box sx={hideOnMobile}>
             <SearchBar />
+          </Box>
+          <Box sx={hideOnMobile}>
+            <UserProfile />
           </Box>
         </Box>
       </Toolbar>
