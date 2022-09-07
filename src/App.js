ebdaac6476455edd1a4c0559d9e4db27c7bdcd3c
Hari <harikrishkk@gmail.com>
2022-09-07 19:49:12 -0400

    commit 11: add toggling to sideList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { Box } from '@mui/material';
 import { appWrapper } from '@styles/styles';
 import AppNavMenu from '@navigation/AppNavMenu';
 import AppContentArea from '@components/AppContentArea';
+import React from 'react';
 
 function App() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
     <Box sx={appWrapper}>
-      <AppNavMenu />
-      <AppContentArea isOpen={false} />
+      <AppNavMenu handleDrawerToggle={handleDrawerToggle} />
+      <AppContentArea isOpen={mobileOpen} />
     </Box>
   );
 }
