c049b6e517247a7cfa795cdab1be2e6ad1d0d8f8
Hari <harikrishkk@gmail.com>
2022-09-07 18:24:45 -0400

    commit 2: install mui, react icons, craco config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
-  return <div className="App">Hello react</div>;
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
 }
 
 export default App;
